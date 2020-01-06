16a4e7a338a95daf9ca29b6161aa8e0953b38bf3
Khizer Younas <m.khizeryounas@gmail.com>
2020-01-06 15:13:56 +0500

    changed file name

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-const db = require("../config/db");
-const locker = require("../src/modules/locker");
+const db = require('../config/db');
+const locker = require('../src/modules/locker');
 /* GET home page. */
-router.get("/", async function(req, res, next) {
-  res.reply({ data: { title: "Express Boilerplate" } });
+router.get('/', async function(req, res, next) {
+  res.reply({ data: { title: 'Express Boilerplate with Actions' } });
 });
 
 module.exports = router;
